13ea7bf306719b7e8ae309cc5644a215bbe86d6e
Julian Calle <jdcallet@unal.edu.co>
2020-07-15 23:57:17 -0500

    Adding responses to each method

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask import request
+from flask import make_response
 import os
 
 import boto3
-from boto3.dynamodb.conditions.Attr import ConditionExpression
+from boto3.dynamodb.conditions import Attr
+import botocore.exceptions
 
 # Variables de entorno
 books_table = os.environ["DYNAMODB_TABLE"]
@@ -12,18 +14,11 @@ books_table = os.environ["DYNAMODB_TABLE"]
 dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table(books_table)
 
-# Inicializando variables Globales
-response = {
-    'statusCode': 502,
-    'body': '',
-    'headers': ''
-}
-
 # Inicializa Flask
 app = Flask(__name__)
-@app.route('/')
 
 # Health check
+@app.route('/')
 def health_check():
     return {'status': 'Ok'}
 
@@ -31,76 +26,164 @@ def health_check():
 @app.route('/resources/books', methods=('GET', 'POST', 'PUT'))
 def books_resource():
     if request.method == 'POST':
-        data = request.form
         try:
-            put_item_response = table.put_item(
-                Item=data
-                ConditionExpression=Attr('id').not_exists()
-            )
-            print(put_item_response)
+            data = request.form.to_dict()
+            print(data)
+            if data["id"] and data["name"]:
+                try:
+                    put_item_response = table.put_item(
+                        Item=data,
+                        ConditionExpression=Attr('id').not_exists()
+                    )
+                    body = {"message": "Book successfully created with id {}".format(data["id"])}
+                    response = make_response(body, 201)
+                    response.headers['Content-Type'] = 'application/json'
+                    return response
+
+                except botocore.exceptions.ClientError:
+                    body = {"error": "The specified id already exists"}
+                    response = make_response(body, 400)
+                    response.headers['Content-Type'] = 'application/json'
+                    return response
+
+                except Exception as e:
+                    print(str(e))
+                    body = {"error": "There was an error creating the book"}
+                    response = make_response(body, 500)
+                    response.headers['Content-Type'] = 'application/json'
+                    return response
+
         except Exception as e:
-            print(e)
+            print(str(e))
+            body = {"error": "You must provide a valid id and name"}
+            response = make_response(body, 400)
+            response.headers['Content-Type'] = 'application/json'
+            return response
 
     if request.method == 'GET':
         id = request.args.get('id')
         author = request.args.get('author')
-        year = request.args.get('year')
-        topic = request.args.get('topic')
         if id:
             try:
-                response["body"] = get_element(id)
-                response["statusCode"] = 200
+                get_item_response = table.get_item(
+                    Key={
+                        'id': id
+                    }
+                )
+                try:
+                    body = {"book": get_item_response["Item"]}
+                    response = make_response(body, 200)
+                    response.headers['Content-Type'] = 'application/json'
+                    return response
+
+                except Exception as e:
+                    print(str(e))
+                    body = {"error": "Book with id {} not found.".format(id)}
+                    response = make_response(body, 404)
+                    response.headers['Content-Type'] = 'application/json'
+                    return response
+
             except Exception as e:
                 print(str(e))
+                body = {"error": str(e)}
+                response = make_response(body, 502)
+                response.headers['Content-Type'] = 'application/json'
+                return response
+
+        elif author:
+            try:
+                scan_response = table.scan(
+                    FilterExpression=Attr('author').eq(author)
+                )
+                body = {"books": scan_response["Items"]}
+                response = make_response(body, 200)
+                response.headers['Content-Type'] = 'application/json'
+                return response
+
+            except Exception as e:
+                print(str(e))
+                body = {"error": str(e)}
+                response = make_response(body, 502)
+                response.headers['Content-Type'] = 'application/json'
+                return response
 
         else:
             try:
-                scan_response = table.scan(
-                    IndexName='id',
-                    FilterExpression=Attr('author').eq(author)
-                )
-                print(scan_response)
+                scan_response = table.scan()
+                body = {"books": scan_response["Items"]}
+                response = make_response(body, 200)
+                response.headers['Content-Type'] = 'application/json'
+                return response
+
             except Exception as e:
                 print(str(e))
+                body = {"error": str(e)}
+                response["statusCode"] = 502
+                return response
 
     if request.method == 'PUT':
-        data = request.form
+        data = request.form.to_dict()
         try:
-            item_id = get_element(id)["id"]
-            data["id"] = item_id
-            put_item_response = table.put_item(
-                Item=data
-            )
-            print(put_item_response)
+            if data["id"]:
+                try:
+                    put_item_response = table.put_item(
+                        Item=data,
+                        ConditionExpression=Attr('id').exists()
+                    )
+                    body = {"message": "Book with id {} was successfully updated".format(data["id"])}
+                    response = make_response(body, 200)
+                    response.headers['Content-Type'] = 'application/json'
+                    return response
+
+                except botocore.exceptions.ClientError:
+                    body = {"error": "The specified id does not exist"}
+                    response = make_response(body, 404)
+                    response.headers['Content-Type'] = 'application/json'
+                    return response
+
+                except Exception as e:
+                    print(str(e))
+                    body = {"error": "There was an error updating the book. Please try again"}
+                    response = make_response(body, 500)
+                    response.headers['Content-Type'] = 'application/json'
+                    return response
+
         except Exception as e:
-            print(e)
+            print(str(e))
+            body = {"error": "You must provide a valid id"}
+            response = make_response(body, 400)
+            response.headers['Content-Type'] = 'application/json'
+            return response
+
+
 
 @app.route('/resources/books/<id>', methods=['DELETE'])
 def book_id_resource(id):
     if request.method == 'DELETE':
-        id = request.args.get('id')
         try:
             delete_item_response = table.delete_item(
                 Key={
                     'id': id
                 }
             )
-            response["body"] = delete_item_response["Attributes"]
-            response["statusCode"] = 200
+            print(delete_item_response)
+            body = {"message": "Book with id {} was successfully deleted".format(id)}
+            response = make_response(body, 200)
+            response.headers['Content-Type'] = 'application/json'
+            return response
 
+        except botocore.exceptions.ClientError:
+            body = {"error": "Book with id {} not found.".format(id)}
+            response = make_response(body, 404)
+            response.headers['Content-Type'] = 'application/json'
             return response
 
         except Exception as e:
             print(str(e))
-
-def get_element(id):
-    get_item_response = table.get_item(
-        Key={
-            'id': id
-        }
-    )
-    return get_item_response["Item"]
-
+            body = {"error": str(e)}
+            response = make_response(body, 502)
+            response.headers['Content-Type'] = 'application/json'
+            return response
 
 
 if __name__ == '__main__':
